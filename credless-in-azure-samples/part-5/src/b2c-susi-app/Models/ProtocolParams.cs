fe973a44ebe2566586bdfd676da04f33a4d34290
ahelland <ahelland@users.noreply.github.com>
2022-08-09 19:17:41 +0200

    Add B2C App Microservice

---
+++ credless-in-azure-samples/part-5/src/b2c-susi-app/Models/ProtocolParams.cs
@@ -0,0 +1,14 @@
+﻿namespace b2c_susi_app.Models
+{
+    public class ProtocolParams
+    {
+        public string? loginHint { get; set; }
+        public string? domainHint { get; set; }
+        public string? locale { get; set; }
+        public string? customParamKey { get; set; }
+        public string? customParamValue { get; set; }
+        public string? policy { get; set; }
+        public string? tokenHint { get; set; }
+
+    }
+}
